663f14aab5aaa926b61f0b5c104c25286cca3156
Peter Marshall <petmshall@gmail.com>
2023-03-22 09:36:03 -0400

    Add missing stdout redirect

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 git clone https://github.com/linux-surface/linux-surface --depth 1
 
-wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.7.tar.xz" | tar xJvf -
+wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.7.tar.xz" -O - | tar xJvf -
 
 cd linux-6.2.7
 
