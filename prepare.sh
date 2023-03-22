47b61fc5f3ca000f60669f8fb72f13096e151d3e
Peter Marshall <petmshall@gmail.com>
2023-03-22 10:48:26 -0400

    Add rsync

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 dnf -y upgrade
-dnf -y install git patch wget xz bc flex bison openssl openssl-devel elfutils-devel elfutils-libelf-devel make clang llvm lld rpmdevtools
+dnf -y install rsync git patch wget xz bc flex bison openssl openssl-devel elfutils-devel elfutils-libelf-devel make clang llvm lld rpmdevtools
