cba052b3750c5b36f12dda4b9118276daaa77cc1
Peter Marshall <petmshall@gmail.com>
2023-03-22 09:56:35 -0400

    Add kernel build depends

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 dnf -y upgrade
-dnf -y install git patch wget xz automake make clang llvm lld rpmdevtools
+dnf -y install git patch wget xz flex bison openssl-devel elfutils-devel elfutils-libelf-devel make clang llvm lld rpmdevtools
