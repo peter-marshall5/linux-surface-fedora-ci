02692d53a1a4e9e23d00e6898a6accf0b84873de
Peter Marshall <petmshall@gmail.com>
2023-03-22 09:40:59 -0400

    Fix ambiguous redirect

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+PATCHES="../linux-surface/patches/6.2/*"
+
 git clone https://github.com/linux-surface/linux-surface --depth 1
 
 wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.7.tar.xz" -qO - | tar xJf -
@@ -9,6 +11,10 @@ cd linux-6.2.7
 
 cp ../.config .
 cp ../.whitelist .
-patch -p1 < ../linux-surface/patches/6.2/*
+
+for p in PATCHES
+do
+patch -p1 < $p
+done
 
 make KCFLAGS="-O3 -mtune=alderlake" LLVM=1 binrpm-pkg
